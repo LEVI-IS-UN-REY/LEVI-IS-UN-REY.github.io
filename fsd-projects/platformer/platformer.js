4518d3dbf62e1d1b16e51c2a19820cba45fb5067
EL REY LEVI <theofficiallevi21@hotmail.com>
2026-02-04 16:05:18 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,24 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    toggleGrid();
 
     // TODO 2 - Create Platforms
-
-
-
-
+createPlatform(500, 650, 200, 20);
+createPlatform(500, 650, 20, 100);
+createPlatform(800, 555, 200, 20);
+createPlatform(1100, 440, 150, 20);
+createPlatform(900, 325, 180, 20);
+createPlatform(500, 200, 200, 20);
     // TODO 3 - Create Collectables
-
-
-
+createCollectable("steve",300,100)
+createCollectable("steve",200, 170, .3,)
+createCollectable("steve",1300, 170)
     
     // TODO 4 - Create Cannons
-
+createCannon("top", 220, 500);
+createCannon("left", 200, 1500);
+createCannon("bottom", 1300, 500);
 
     
     
